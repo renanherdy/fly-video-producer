7908ddc59523fa6bf25d3637d52119b45743dcd6
Renan Herdy e Silva <renanherdy@gmail.com>
2019-10-06 21:58:46 -0300

    issue #10 autoslice better logic

--- src/videoSlicer.js
+++ src/videoSlicer.js
@@ -12,7 +12,7 @@ const sliceMinLength = 3;
 const maxSliceQty = 8;
 
 async function autoSlice() {
-  // cleanOutputDirectory();
+  cleanOutputDirectory();
   const inputFiles = await loadInputFiles();
   const slicingData = await calculateSlicingData(inputFiles);
   await renderToFile(slicingData);
@@ -85,15 +85,15 @@ async function saveSlice(inputFile, slice) {
     ffmpegCommand
       .setStartTime(slice.start)
       .setDuration(inputFile.slices.duration)
-    // .saveToFile(slice.outputPath)
-    // .on('end', () => {
-    console.log('slice');
-    console.log(slice);
-    resolve(slice);
-    // })
-    // .on('error', (err) => {
-    //   reject(err);
-    // });
+      .saveToFile(slice.outputPath)
+      .on('end', () => {
+        console.log('slice');
+        console.log(slice);
+        resolve(slice);
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
   });
 }
 
@@ -193,68 +193,3 @@ function cleanOutputDirectory() {
     }
   });
 }
-// function autoSlice(){
-//   console.log('slice world!');
-//   const command = ffmpeg('./source-files/12-02-51-GOPR8939.mp4')
-//   .setStartTime(40)
-//   .setDuration(5);
-//   .saveToFile('./output-files/slice2.mp4')
-//   command
-//   .saveToFile('./output-files/slice2.mp4');
-//   const command2 = ffmpeg()
-//   .addInput('./output-files/slice1.mp4')
-//   .mergeAdd('./output-files/slice2.mp4')
-//   command2
-//   .mergeToFile('./output-files/slice3.mp4', './output-files/');
-// }
-
-// function autoSlice(element) {
-//   console.log('auto slicing object');
-//   const command = ffmpeg(element.localPath)
-//     .setStartTime(20)
-//     .setDuration(5);
-//   command
-//     .saveToFile('./output-files/slice1.mp4')
-//     .on('end', () => {
-//       console.log('slice 1 saved');
-//       const command2 = ffmpeg(element.localPath)
-//       command2
-//         .setStartTime(40)
-//         .setDuration(5);
-//       command2
-//         .saveToFile('./output-files/slice2.mp4')
-
-//         .on('end', () => {
-//           console.log('slice 2 saved');
-//           const command3 = ffmpeg(element.localPath)
-//           command3
-//             .setStartTime(55)
-//             .setDuration(5);
-//           command3
-//             .saveToFile('./output-files/slice3.mp4')
-//             .on('end', () => {
-//               console.log('slice 3 saved');
-//               const command4 = ffmpeg(element.localPath)
-//               command4
-//                 .setStartTime(65)
-//                 .setDuration(5);
-//               command4
-//                 .saveToFile('./output-files/slice4.mp4')
-//                 .on('end', () => {
-//                   console.log('slice 4 saved');
-//                   const command5 = ffmpeg()
-//                     .addInput('./output-files/slice1.mp4')
-//                     .mergeAdd('./output-files/slice2.mp4')
-//                     .mergeAdd('./output-files/slice3.mp4')
-//                     .mergeAdd('./output-files/slice4.mp4')
-//                   command5
-//                     .mergeToFile('./output-files/autoSliced.mp4', './output-files/')
-//                     .on('end', () => {
-//                       console.log('autoSliced saved');
-//                     });
-//                 });
-//             });
-//         });
-//     });
-
-// }
